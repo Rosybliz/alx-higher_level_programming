651cf5084db21475240d5e37e86dd61da3e26309
Rosybliz <bchoco961@gmail.com>
2023-10-23 09:28:26 -0700

    My commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 def safe_print_list(my_list=[],x=0):
-    a = 0
     try:
-        for i in range(x):
+        for item in my_list[]:
+            if isinstance(item, int,str,float,bool,set,dict,list,tuple)
             print(my_list[i], end=" ")
-            a += 1
+            x += 1
     except IndexError:
         pass
     print()
