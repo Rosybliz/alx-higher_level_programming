c6bf1f4faf8e4c90bfebaf28a5a277f6270acee8
Rosybliz <bchoco961@gmail.com>
2023-10-24 10:13:30 -0700

    Private instance

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,8 +5,10 @@
 
 class Square:
     """This Square class will be used to create a square shape"""
+
     def __init__(self, size):
         """Instantiation of the attributes
+
         Args:
             size(int): this is the size of the square instantiated to private
         """
