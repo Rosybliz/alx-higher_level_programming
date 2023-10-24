4d00e37db3536f808716d8634b7b012821c4f167
Rosybliz <bchoco961@gmail.com>
2023-10-24 12:54:46 -0700

    My

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""A class that contains the definition of a sqaure"""
+
+
+class Square:
+    """This Square class will be used to create a square shape"""
+
+    def __init__(self, size=0):
+
+        """instantiation with optional size =0
+
+        Args:
+            size(int): the size of the square
+        """
+        self.size = size
+
+        def area(self):
+            """Calculates the area of the square"""
+            return self.__size ** 2
+
+        @property
+        def size(self):
+            """Returns the size of the square"""
+            return self.___size
+
+        @size.setter
+        def size(self, value):
+            """Sets the size of the square"""
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+                else:
+                self.__size = value
