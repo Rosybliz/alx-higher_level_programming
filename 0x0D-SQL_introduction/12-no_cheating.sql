93b79e53507b08afca53fcfe134f9e3177d8a975
Rosybliz <bchoco961@gmail.com>
2023-12-13 03:45:56 -0800

    for 12,13,14

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- update Bob's score to 10
+--you're not allowed to use bob's id value, only name field
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes all record of score <= 5 in the second_table
+DELETE FROM `second_table`
+WHERE `score` <= 5;
