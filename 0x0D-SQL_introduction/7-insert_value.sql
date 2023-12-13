7466051e0e32e1e05aced11d681bc547a5f6db47
Rosybliz <bchoco961@gmail.com>
2023-12-13 02:52:23 -0800

    7 commit

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
 -- a script that inserts a new row in the table firsttable
-INSERT INTO first_table('id', 'name')
-VALUES(89, 'Best School');
+INSERT INTO `first_table`
+(`id`, `name`)
+VALUES (89, 'Best School');
