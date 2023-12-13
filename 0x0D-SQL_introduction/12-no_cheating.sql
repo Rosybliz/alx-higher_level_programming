aeea75a991a4432834be9d8ac4e4e30980225285
Rosybliz <bchoco961@gmail.com>
2023-12-13 04:05:49 -0800

    others

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -2,4 +2,7 @@
 --you're not allowed to use bob's id value, only name field
 UPDATE `second_table`
 SET `score` = 10
-WHERE `name` = 'Bob';
+WHERE `score` =
+(SELECT `score`
+FROM `second_table`
+WHERE `name` = 'Bob');
